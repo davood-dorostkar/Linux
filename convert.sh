87bba1d8abcbf68e04ac5610c190b1a024b7a1a5
Davood Dorostkar <44213441+davood-dorostkar@users.noreply.github.com>
2022-02-28 19:08:55 +0330

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+speed=20
 while getopts s: flag
 do
     case "${flag}" in
@@ -7,26 +7,40 @@ do
     esac
 done
 
-rm audio -r
-rm speed -r
-rm list.txt
-rm result.mp4
+rm audio -r &> /dev/null
+rm speed -r &> /dev/null
+rm list.txt &> /dev/null
+rm result.mp4 &> /dev/null
 mkdir audio
 mkdir speed
 files=`find . -name "*.mp4" -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $3}'`
-var= 1
-((var++))
+totalFilesCount=$(find . -name "*.mp4" | wc -l)
+
+var=1
+# ((var++))
 for f in $files; do
+  echo "Processing: " $var "of" $totalFilesCount
+  echo $((100*$var/totalFilesCount)) "%"
   Path=`realpath $f`
-  echo  "rpth=" $Path
+  echo  "Current File:" $Path
   BaseName="$(basename -- $Path)"
-  echo "base= $BaseName"
-  ffmpeg -hwaccel cuda -i $Path -c copy -an audio/$BaseName
-  ffmpeg -hwaccel cuda -i audio/$BaseName -filter:v "setpts=PTS/$speed" "speed/$var.mp4"
+  # echo "base= $BaseName"
+  ffmpeg -hide_banner -loglevel error -hwaccel cuda -i $Path -c copy -an audio/$BaseName
+  if [ $? -ne 0 ]
+  then
+    echo "Error in audio of" $BaseName
+    echo "Error in audio of" $BaseName  >> errorLog.txt 
+  fi
+  ffmpeg -hide_banner -loglevel error -hwaccel cuda -i audio/$BaseName -filter:v "setpts=PTS/$speed" "speed/$var.mp4"
+  if [ $? -ne 0 ]
+  then
+    echo "Error in speed up of" $BaseName
+    echo "Error in speed up of" $BaseName  >> errorLog.txt 
+  fi
   ((var++))
   done
 
-
+echo "Merging all files together"
 speedupfiles=`find ./speed -type f -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $3}'`
 for f in $speedupfiles; do
   Path=`realpath $f`
@@ -39,10 +53,15 @@ for f in $speedupfiles; do
   # echo  file "'$Path'" >> list.txt
   done
 
-ffmpeg -hwaccel cuda -f concat -safe 0 -i list.txt -c copy result.mp4
-rm speed -r
-rm audio -r
-rm list.txt
-echo  "finished"
+ffmpeg -hide_banner -loglevel error -hwaccel cuda -f concat -safe 0 -i list.txt -c copy result.mp4
+if [ $? -ne 0 ]
+then
+  echo "Error in concatenation"
+  echo "Error in concatenation" >> errorLog.txt 
+fi
+# rm speed -r
+# rm audio -r
+# rm list.txt
+echo  "Finished Rendering"
 
 
